b5a9319f697d33f2e4d40e819f3095e07577905b
Lucas Cardoso <lucas.cardosopessoal@hotmail.com>
2021-08-24 19:36:15 -0300

    webshift - Springboot

---
+++ webshift/src/main/java/br/com/fiap/webshift/WebshiftApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.webshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshiftApplication.class, args);
+	}
+
+}
