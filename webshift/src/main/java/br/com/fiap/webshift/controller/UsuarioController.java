6062b115a5f8b69d3f773d6a8a3d0fc51f1421a0
Lucas Cardoso <lucas.cardosopessoal@hotmail.com>
2021-09-06 23:28:28 -0300

    UsuarioController

--- webshift/src/main/java/br/com/fiap/webshift/controller/UsuarioController.java
+++ webshift/src/main/java/br/com/fiap/webshift/controller/UsuarioController.java
@@ -6,28 +6,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.webshift.model.UsuarioModel;
 import br.com.fiap.webshift.repository.UsuarioRepository;
 
 @RestController
+@RequestMapping("/usuario")
 @CrossOrigin("*")
 public class UsuarioController {
 
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 
-	@GetMapping("/usuario")
-	public ResponseEntity<List<UsuarioModel>> getAll() {
-
+	@GetMapping
+	public ResponseEntity<List<UsuarioModel>> findAll() {
 		List<UsuarioModel> lista = usuarioRepository.findAll();
-
-		if (lista.isEmpty()) {
-			return ResponseEntity.status(404).build();
-		}
-
 		return ResponseEntity.ok(lista);
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UsuarioModel> findById(@PathVariable("id") int id){
+		UsuarioModel model = usuarioRepository.findById(id).get();
+		if (model == null) {
+			return ResponseEntity.status(404).build();
+		}
+		return ResponseEntity.ok(model);
+	}
+	
 
 }
