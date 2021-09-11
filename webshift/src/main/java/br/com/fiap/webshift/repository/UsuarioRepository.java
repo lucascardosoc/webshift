a9ca886a6e2230e347fe627704ef00640825862d
Lucas Cardoso <lucas.cardosopessoal@hotmail.com>
2021-09-10 22:04:18 -0300

    UsuarioRepository

--- webshift/src/main/java/br/com/fiap/webshift/repository/UsuarioRepository.java
+++ webshift/src/main/java/br/com/fiap/webshift/repository/UsuarioRepository.java
@@ -1,6 +1,8 @@
 package br.com.fiap.webshift.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.fiap.webshift.model.UsuarioModel;
@@ -8,4 +10,9 @@ import br.com.fiap.webshift.model.UsuarioModel;
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Integer> {
 	
+	public UsuarioRepository findByEmailAndSenha(String email, String senha);
+	
+	@Query(value="SELECT count(ID_USUARIO) as total FROM USUARIO"
+			+ "WHERE idade = :idade", nativeQuery = true)
+	public long getTotalByIdade(@Param("idade") int idade);
 }
