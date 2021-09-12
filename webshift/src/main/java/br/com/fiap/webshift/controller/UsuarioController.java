ce3ee3b24ab7d671dfad6012def8d2ded2cad00a
Lucas Cardoso <lucas.cardosopessoal@hotmail.com>
2021-09-11 21:21:10 -0300

    UsuarioController

--- webshift/src/main/java/br/com/fiap/webshift/controller/UsuarioController.java
+++ webshift/src/main/java/br/com/fiap/webshift/controller/UsuarioController.java
@@ -1,6 +1,5 @@
 package br.com.fiap.webshift.controller;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +9,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.fiap.webshift.model.UsuarioModel;
 import br.com.fiap.webshift.repository.UsuarioRepository;
